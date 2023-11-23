46e9107d5bfc0d4ae7b9f7fbc3a5d6db81bbb190
Joekingu <roussel.maximeg@gmail.com>
2023-11-23 09:17:00 +0100

    exemple de communication entre frontend et backend

---
+++ backend/service-emprunt/serveur.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3001; // Choose a port for your backend
+
+// Middleware to parse incoming JSON data
+app.use(express.json());
+
+// Define a sample endpoint
+app.get('/api/data', (req, res) => {
+  const data = { message: 'Hello from the backend!' };
+  res.json(data);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Backend server listening at http://localhost:${port}`);
+});
--- frontend/app.js
+++ frontend/app.js
@@ -17,3 +17,31 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+// Assume this code is in your frontend JavaScript file
+const fetchData1 = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/data');
+    const data = await response.json();
+    console.log(data);
+    // Now you can use the data in your frontend
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+fetchData1();
+
+// Assume this code is in your frontend JavaScript file
+const fetchData2 = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/data');
+    const data = await response.json();
+    console.log(data);
+    // Now you can use the data in your frontend
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+fetchData2();
