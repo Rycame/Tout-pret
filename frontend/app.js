eae6955844896b26c3ad8ed4b613a63172c7fae4
Joekingu <roussel.maximeg@gmail.com>
2023-11-23 09:03:15 +0100

    initialisation du site

---
+++ frontend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
